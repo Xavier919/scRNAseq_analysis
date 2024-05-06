40e9a5efacf82786681ce36632674caa576b131d
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-05-06 14:26:56 -0400

    initial commit

--- modules/autoencoder_model.py
+++ modules/autoencoder_model.py
@@ -7,26 +7,26 @@ class Autoencoder(nn.Module):
 
         self.encoder = nn.Sequential(
             nn.Linear(13492, 4096),
-            nn.PReLU(True),
+            nn.GELU(),
             #nn.Dropout(dropout_rate), 
             nn.Linear(4096, 1024),
-            nn.PReLU(True),
+            nn.GELU(),
             #nn.Dropout(dropout_rate),  
             nn.Linear(1024, 256),
-            nn.PReLU(True),
+            nn.GELU(),
             #nn.Dropout(dropout_rate),
             nn.Linear(256, 32)
         )
 
         self.decoder = nn.Sequential(
             nn.Linear(32, 256),
-            nn.PReLU(True),
+            nn.GELU(),
             #nn.Dropout(dropout_rate),  
             nn.Linear(256, 1024),
-            nn.PReLU(True),
+            nn.GELU(),
             #nn.Dropout(dropout_rate),  
             nn.Linear(1024, 4096),
-            nn.PReLU(True),
+            nn.GELU(),
             #nn.Dropout(dropout_rate),  
             nn.Linear(4096, 13492),
         )
