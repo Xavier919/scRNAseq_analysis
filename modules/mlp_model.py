2e8756032d77a7e791a12c5a425e3c3e95818c94
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-05-11 09:55:50 -0400

    initial commit

--- modules/mlp_model.py
+++ modules/mlp_model.py
@@ -8,6 +8,7 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.layers = nn.ModuleList()
         self.layers.append(nn.Linear(input_size, hidden_sizes[0]))
+        self.dropout = nn.Dropout()
         for i in range(1, len(hidden_sizes)):
             self.layers.append(nn.Linear(hidden_sizes[i - 1], hidden_sizes[i]))
         self.layers.append(nn.Linear(hidden_sizes[-1], output_size))
@@ -15,6 +16,7 @@ class MLP(nn.Module):
     def forward(self, x):
         for layer in self.layers[:-1]:
             x = F.relu(layer(x))  
+            x = self.dropout(x)
         x = self.layers[-1](x) 
         return x
     
