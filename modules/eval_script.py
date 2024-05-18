9abd7f192e96b68aa7ce917d85f79eb4b6fbf640
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-05-17 22:45:16 -0400

    initial commit

--- modules/eval_script.py
+++ modules/eval_script.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         test_data = torch.Tensor(X_test)
-        test_labels = torch.Tensor(Y_test)
+        test_labels = Y_test
         test_dataset = TensorDataset(test_data, test_labels)
         test_loader = DataLoader(test_dataset, batch_size=1)
 
