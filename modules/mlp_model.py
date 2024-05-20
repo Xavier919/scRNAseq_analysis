0cfb86c4096cdaf485501ecca5120baaf7f07686
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-05-20 18:13:39 -0400

    initial commit

--- modules/mlp_model.py
+++ modules/mlp_model.py
@@ -31,3 +31,41 @@ class SiameseMLP(nn.Module):
         processed_b = self.base_network(input_b)
         distance = torch.norm(processed_a - processed_b, p=2, dim=1)
         return distance
+    
+
+
+class MLP(nn.Module):
+    def __init__(self, input_size):  
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.encoder = nn.Sequential(
+            nn.Linear(self.input_size, 4096),
+            nn.GELU(),
+            nn.Linear(4096, 2048),
+            nn.GELU(),
+            nn.Linear(2048, 512),
+            nn.GELU(),
+            nn.Linear(512, 128),
+        )
+        self.dual = nn.Sequential(
+            nn.Linear(128, 32),
+            nn.GELU(),
+        )
+        
+    def forward(self, x):
+        x = self.encoder(x)
+        x1 = self.dual(x)
+        x2 = self.dual(x)
+        return x1, x2
+    
+class SiameseMLP(nn.Module):
+    def __init__(self, base_network):
+        super(SiameseMLP, self).__init__()
+        self.base_network = base_network
+    
+    def forward(self, input_a, input_b):
+        type_a, pheno_a = self.base_network(input_a)
+        tybe_b, pheno_b = self.base_network(input_b)
+        type_distance = torch.norm(type_a - tybe_b, p=2, dim=1)
+        pheno_distance = torch.norm(pheno_a - pheno_b, p=2, dim=1)
+        return type_distance, pheno_distance
