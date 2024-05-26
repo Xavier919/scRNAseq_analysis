c6435b3abea93f5a4d1bfb03be5fc1fef6061446
Xavier919 <xavier.lapointe@usherbrooke.ca>
2024-05-26 19:51:23 -0400

    initial commit

--- modules/mlp_model.py
+++ modules/mlp_model.py
@@ -39,19 +39,19 @@ class SiameseMLP(nn.Module):
     
 
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_sizes, output_size=32):
+    def __init__(self, input_size, hidden_sizes, output_size=32, activation=nn.GELU()):
         super(MLP, self).__init__()
         self.layers = nn.ModuleList()
         self.layers.append(nn.Linear(input_size, hidden_sizes[0]))
         self.dropout = nn.Dropout()
-        self.gelu = nn.GELU()
+        self.activation = activation
         for i in range(1, len(hidden_sizes)):
             self.layers.append(nn.Linear(hidden_sizes[i - 1], hidden_sizes[i]))
         self.layers.append(nn.Linear(hidden_sizes[-1], output_size))
 
     def forward(self, x):
         for layer in self.layers[:-1]:
-            x = self.gelu(layer(x))  
+            x = self.activation(layer(x))  
             #x = self.dropout(x)
         x = self.layers[-1](x) 
         return x
